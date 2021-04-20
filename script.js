5d0f3ec95e2731e1629e355925702b731217e6ba
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-04-20 07:39:46 -0400

    api functionality working

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
 //Link API
-  //Get Categories from API
+//Get Categories from API
 async function getCategories() {
- try {
-  const categoryURL = 'https://opentdb.com/api_category.php'; 
-   let categoryResponse = await axios.get(categoryURL);
+  try {
+    const categoryURL = 'https://opentdb.com/api_category.php';
+    let categoryResponse = await axios.get(categoryURL);
     //console.log(categoryResponse.data.trivia_categories);
-   setOptions(categoryResponse.data.trivia_categories);
-   return categoryResponse.data.trivia_categories;
+    setOptions(categoryResponse.data.trivia_categories);
+    return categoryResponse.data.trivia_categories;
   } catch (error) {
     console.error(error);
   }
 }
 getCategories();
-  //Append to DOM
-  function setOptions(list) {
-    //console.log(list)
-    const selectTag = document.querySelector('#select-category')
-    list.forEach((category) => {
-      //console.log(category)
-      const optionTag = document.createElement('option')
-      optionTag.textContent = category.name;
-      optionTag.value = category.name;
-      selectTag.append(optionTag)
-    })
-    return list
+//Append to DOM
+function setOptions(list) {
+  //console.log(list)
+  const selectTag = document.querySelector('.select-category')
+  list.forEach((category) => {
+    //console.log(category)
+    const optionTag = document.createElement('option')
+    optionTag.textContent = category.name;
+    optionTag.value = category.id;
+    selectTag.append(optionTag)
+  })
+  return null;
+}
+
+function getValues(e) {
+  e.preventDefault()
+  //console.log(e.target);
+  const name = document.querySelector('input').value;
+  //console.log(name);
+  const categoryValue = document.querySelector('.select-category').value;
+  //console.log(categoryValue)
+  const difficultyLevel = document.querySelector("#select-difficulty").value;
+  //console.log(difficultyLevel)
+  let valueArray = [name, categoryValue, difficultyLevel];
+  getQuestions(valueArray);
   }
 
-  function getValue(e) {
-    e.preventDefault()
-    const optionValue = document.querySelector('#select-category').value
-    //console.log(optionValue)
-    getCategory(optionValue)
-    return optionValue
-  }
 
-  const form = document.querySelector('form')
-form.addEventListener("submit", getValue)
+
+const form = document.querySelector('form')
+form.addEventListener("submit", getValues)
+
+//Display Info on GamePage
+async function getQuestions(valueArray) {
+  const questionsURL = `https://opentdb.com/api.php?amount=10&category=${valueArray[1]}&difficulty=${valueArray[2]}`
+  try {
+    let questionDataResponse = await axios.get(questionsURL);
+    console.log(questionDataResponse.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// function displayDifficulty (){
+//   let categoryH2 = document.selectElementById('category');
+//   categoryH2.innerText = `Category: ${categoryValue}`;
+//   console.log(categoryH2);
+
+//Append Category to GamePage
+
 
 
 //Build out HTML elements/structure
