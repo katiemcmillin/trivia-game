e5522565398b61560fbf38b0e604c00589fcf555
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-04-21 11:42:30 -0400

    game functionality works

---
+++ final.js
@@ -0,0 +1,1 @@
+document.querySelector('h1').textContent = `Final Score: ${localStorage.getItem('finalScore')}`;
--- script-game-page.js
+++ script-game-page.js
@@ -1,95 +1,113 @@
 let totalScore = 0;
 let turn = 0;
-let results = [];
+
 document.querySelector('#score').textContent = `Score: ${totalScore}`;
 
 async function getQuestions() {
   const questionsURL = `https://opentdb.com/api.php?amount=10&category=${localStorage.categoryId}&difficulty=${localStorage.difficulty}`;
   try {
     let questionDataResponse = await axios.get(questionsURL);
-    currentQuestion(questionDataResponse.data, turn);
+    localStorage.setItem('apiData', JSON.stringify(questionDataResponse.data));
+    currentQuestion(JSON.parse(localStorage.getItem('apiData')), turn);
+
   } catch (error) {
     console.error(error);
   }
 }
 getQuestions();
-console.log(results)
 function makePrettyString(str) {
   str = str.replace(/&quot;/g, '"');
   str = str.replace(/&#039;/g, "'");
   str = str.replace(/&ldquo;/g, '"');
   str = str.replace(/&amp;/g, '&');
   str = str.replace(/&rdquo;/g, '"');
+  str = str.replace(/&eacute;/g, 'è');
+  str = str.replace(/&iacute;/g, 'ì');
+  str = str.replace(/&uuml;/g, 'ü');
+  str = str.replace(/&ecirc;/g, 'î');
+  str = str.replace(/&ntilde;/g, 'ñ');
   return str;
 }
 
 document.querySelector('#category').textContent = `Category: ${localStorage.categoryText}`;
+document.querySelector('#answerTitle').textContent = `${localStorage.name}, select the correct answer`
 
 function currentQuestion(data, turn) {
-    let question = `${makePrettyString(data.results[turn].question)}`
-    document.querySelector('#question').textContent = `Question: ${question}`;
-    let allAnswersArray = [];
-    const correctAnswer = `${makePrettyString(data.results[0].correct_answer)}`;
-    let incorrectAnswersArray = data.results[0].incorrect_answers;
+  let question = `${makePrettyString(data.results[turn].question)}`
+  document.querySelector('#question').textContent = `Question: ${question}`;
+  document.querySelectorAll('input[type=radio').forEach(e => e.remove());
+  document.querySelectorAll('label').forEach(e => e.remove());
+  let allAnswersArray = [];
+  const correctAnswer = `${makePrettyString(data.results[turn].correct_answer)}`;
+  let incorrectAnswersArray = data.results[turn].incorrect_answers;
+  for (i = 0; i < incorrectAnswersArray.length; i++) {
+    incorrectAnswersArray[i] = makePrettyString(incorrectAnswersArray[i]);
+  };
+  allAnswersArray = [correctAnswer, ...incorrectAnswersArray];
+  allAnswersArray = allAnswersArray.sort(() => Math.random() - 0.5);
+  // Found syntax for radio buttons here: https://www.tutorialspoint.com/how-to-dynamically-create-radio-buttons-using-an-array-in-javascript
+  //https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
+  allAnswersArray.forEach(answer => {
+    let answerLabel = document.createElement('label');
+    let answerButton = document.createElement('input');
+    answerLabel.setAttribute('for', answer)
+    answerLabel.textContent = answer;
+    answerButton.value = answer;
+    answerButton.name = 'choice';
+    answerButton.type = 'radio';
+    if (answer === correctAnswer) {
+      answerButton.classList = 'correct-answer';
+    } else {
+      answerButton.classList = 'wrong-answer';
+    }
+    document.querySelector('#answer').append(answerLabel);
+    document.querySelector('#answer').append(answerButton);
+  });
 
-    for (i = 0; i < incorrectAnswersArray.length; i++) {
-      incorrectAnswersArray[i] = makePrettyString(incorrectAnswersArray[i]);
-    };
-    allAnswersArray = [correctAnswer, ...incorrectAnswersArray];
-    allAnswersArray = allAnswersArray.sort(() => Math.random() - 0.5);
-    // Found syntax for radio buttons here: https://www.tutorialspoint.com/how-to-dynamically-create-radio-buttons-using-an-array-in-javascript
-    //https://www.javascripttutorial.net/javascript-dom/javascript-radio-button/
-    allAnswersArray.forEach(answer => {
-      let answerLabel = document.createElement('label');
-      let answerButton = document.createElement('input');
-      answerLabel.setAttribute('for', answer)
-      answerLabel.textContent = answer;
-      answerButton.value = answer;
-      answerButton.name = 'choice';
-      answerButton.type = 'radio';
-      if (answer === correctAnswer) {
-        answerButton.classList = 'correct-answer';
-      } else {
-        answerButton.classList = 'wrong-answer';
-      }
-      document.querySelector('#answer').append(answerLabel);
-      document.querySelector('#answer').append(answerButton);
-    });
-    
 
 }
 
 const btn = document.querySelector('#btn');
-    // handle click button
-    btn.onclick = function selectAnswer () {
-      const choices = document.querySelectorAll('input[name="choice"]');
-      let selectedValue;
-      for (const choice of choices) {
-        if (choice.checked) {
-          selectedValue = choice.value;
-          turn++
-          console.log(turn)
-          scoreTracker(choice.classList.value);
-          //scoreTracker(selectedValue);
-          break
-        }
+// handle click button
+  btn.onclick = function selectAnswer() {
+    const choices = document.querySelectorAll('input[name="choice"]');
+    let selectedValue;
+    for (const choice of choices) {
+      if (choice.checked) {
+        selectedValue = choice.value;
+        turn++
+        console.log(turn)
+        scoreTracker(choice.classList.value);
+        currentQuestion(JSON.parse(localStorage.getItem('apiData')), turn)
+        //scoreTracker(selectedValue);
+        break
       }
     }
+  }
+
 
 
 function scoreTracker(value) {
-      if (value === 'correct-answer') {
-      totalScore++;
-        document.querySelector('#score').textContent = `Score: ${totalScore}`;
-        console.log(totalScore)
-      //alert(`You chose wisely! Score: ${totalScore}`);
+  
+  if (value === 'correct-answer') {
+    totalScore++;
     
+    document.querySelector('#selection-message').textContent = `Correct!`
+    document.querySelector('#score').textContent = `Score: ${totalScore}`;
+    console.log(totalScore)
     
-      } else {
-        console.log(totalScore)
-      //alert(`You chose poorly! Score: ${totalScore}`);
-    }
 
+
+  } else {
+    document.querySelector('#selection-message').textContent = `Incorrect!`
+    console.log(totalScore)
+   
+  }
+  if (turn > 9) {
+    localStorage.setItem('finalScore', `${totalScore}/10`)
+    window.location.replace("./final-score.html");
+    
+    }
 }
 
 
